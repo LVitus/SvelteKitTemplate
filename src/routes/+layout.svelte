c6f94c35b6fb10b981876a14e8945c276df434ed
Leonard Vitus WInkelhaus-Elsing <leonard@LeonardsMBP.fritz.box>
2024-06-22 09:45:13 +0200

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>
+	<nav>
+		<ul class="flex justify-evenly">
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main class="flex-grow">
+	<slot />
+</main>
+
+<footer class=" flex justify-center">
+	<p>&copy; 2024</p>
+</footer>
+
+<style></style>
