c8274bfb816a72ea37d5a3ae69adde38b24408d3
Leonard Vitus WInkelhaus-Elsing <leonard@LeonardsMBP.ggm>
2024-07-02 14:10:17 +0200

    updted auth

---
+++ src/routes/(unrestricted)/+layout.ts
@@ -0,0 +1,15 @@
+import { session } from '$lib/database/session.svelte';
+import type { LayoutLoad } from './$types';
+
+export const ssr = false;
+
+export const load: LayoutLoad = (async ({ data }) => {
+
+    await session.init(data.token);
+
+    return {
+        ses: session,
+        db: session.database,
+        user: null
+    };
+});
